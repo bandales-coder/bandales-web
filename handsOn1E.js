cc3a1759069df78fcc5e56500b76f4792dafc2bf
sanrelnashdelarosa31-sudo <230487901+bandales-coder@users.noreply.github.com>
2025-10-10 13:48:18 +0800

    midterm handsOn
    finish html

---
+++ handsOn1E.js
@@ -0,0 +1,10 @@
+function computePower(){
+
+    let Wattage = Number (document.getElementById("wattage").value);
+    let Hours = Number (document.getElementById("hours").value);
+
+    if(Wattage && Hours)
+    {console.log()
+        
+    }
+}
